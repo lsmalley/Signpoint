ddb6c3dba46a2853af8c7b25508a6533394dafe8
Ben-Donnellan35 <Ben.Donnellan@outlook.com>
2018-04-12 14:16:41 +0100

    Images in database

---
+++ Signpoint/Signpoint/Classes/ImageHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Signpoint.Classes
+{
+    public class ImageHelper
+    {
+        public static string ImagePath(string Path)
+        {
+            return "~/Content/" + Path;
+        }
+    }
+}
---
+++ Signpoint/Signpoint/Models/Image.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Signpoint.Models
+{
+    public class Image
+    {
+        public int ID { get; set; }
+        public string ImgLocation { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class ImageDBContext : DbContext
+    {
+        public DbSet<Image> Images { get; set; }
+    }
+}
